fdbba08a5cbe1d3c06a8c78cb885e4f59e785584
YanielRamos <yanielramos0504@gmail.com>
2023-02-28 18:32:05 +0000

    adding file

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - starting point
+ * @str: variable
+ * Return: 0 always
+ */
+
+void puts_half(char *str)
+{
+	int a = 0;
+	int b, c;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		a++;
+	}
+	if (a % 2 == 0)
+	{
+		b = a / 2;
+	}
+	else
+	{
+		b = (a - 1) / 2;
+	}
+	for (c = b; c < a; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
