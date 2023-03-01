54438a10f9586a663d13aa114cc6ae0f6cbaa051
YanielRamos <yanielramos0504@gmail.com>
2023-03-01 19:23:00 +0000

    adding file 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: variable
+ * @s2: variable
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+		{
+			break;
+		}
+	}
+	if (s1[a] < s2[a])
+	{
+		return (-15);
+	}
+	else if (s1[a] > s2[a])
+	{
+		return (15);
+	}
+	else
+	{
+	return (0);
+	}
+}
