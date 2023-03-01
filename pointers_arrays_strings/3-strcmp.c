851c24c75ee880066ce96a9a93cd34d52761266e
YanielRamos <yanielramos0504@gmail.com>
2023-03-01 19:41:02 +0000

    adding file 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -15,19 +15,8 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[a] != s2[a])
 		{
-			break;
+			return (s1[a] - s2[a]);
 		}
 	}
-	if (s1[a] < s2[a])
-	{
-		return (-13);
-	}
-	else if (s1[a] > s2[a])
-	{
-		return (13);
-	}
-	else
-	{
-	return (0);
-	}
+	return (s1[a] - s2[a]);
 }
