4e0c62f1b4d01b4005fa86f553efbeedf092548e
YanielRamos <yanielramos0504@gmail.com>
2023-03-01 18:13:08 +0000

    file that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *p = dest;
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		*p  = src[i];
-	}
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[i] = src[i];
+    }
+    for (; i < n; i++)
+    {
+        dest[i] = '\0';
+    }
 	return (dest);
 }
