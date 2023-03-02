2bd2abf35e7bbe69dea698bee8b61fba44b47def
YanielRamos <yanielramos0504@gmail.com>
2023-03-02 19:56:46 +0000

    adding sixth file

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,44 +2,45 @@
 
 /**
  * cap_string - Function that capitalizes all words of a string
- * @s: string with info
- * Return: s to have the result
+ * @str: string with info
+ * Return: str to have the result
  */
-
-#include <stdio.h>
 
 char *cap_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] -= ('a' - 'A');
+	}
 
-    if (str[0] >= 'a' && str[0] <= 'z') {
-        str[0] -= ('a' - 'A');
-    }
+	while (str[i] != '\0')
+	{
 
-    while (str[i] != '\0') {
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' ||
+			str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' ||
+			str[i] == '{' || str[i] == '}')
+		{
+			i++;
+		while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' ||
+			str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' ||
+			str[i] == '{' || str[i] == '}')
+		{
+			i++;
+		}
 
-        if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' ||
-            str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' ||
-            str[i] == '{' || str[i] == '}') {
-
-            i++;
-
-            while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
-                   str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' ||
-                   str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' ||
-                   str[i] == '{' || str[i] == '}') {
-                i++;
-            }
-
-            if (str[i] >= 'a' && str[i] <= 'z') {
-                str[i] -= ('a' - 'A');
-            }
-        } else {
-            i++;
-        }
-    }
-
-    return str;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= ('a' - 'A');
+		}
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (str);
 }
