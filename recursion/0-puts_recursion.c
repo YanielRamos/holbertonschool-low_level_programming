5d4f2708f8ac39c86b06d5d52b7008195481c280
YanielRamos <yanielramos0504@gmail.com>
2023-03-07 15:11:26 +0000

    adding file

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * _puts_recursion - function that prints a string
  * @s: string
  * Return: program is running great
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                _putchar('\n');
-                return;
-        }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 	else
 	{
 		_putchar(*s);
