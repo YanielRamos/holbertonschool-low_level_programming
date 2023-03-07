6df1ca82543e74c111eb6734fc27a7928b6a98ec
YanielRamos <yanielramos0504@gmail.com>
2023-03-07 20:15:07 +0000

    adding 4 file

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - function that return the value of x
+ * @x: base
+ * @y: power
+ * Return: 1 is succesful
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
