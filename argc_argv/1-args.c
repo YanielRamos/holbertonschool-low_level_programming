887df4e485ba81cbcd61aac4f04b33b3fefcfaa7
YanielRamos <yanielramos0504@gmail.com>
2023-03-10 14:53:50 +0000

    adding file 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,13 +2,13 @@
 
 /**
  * main - program that prints the number of arguments
- *
- *
+ * @argc: function that counts arguments
+ * @argv: function that prints arguments
  * Return: 0 succesful
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-	return 0;
+	return (0);
 }
