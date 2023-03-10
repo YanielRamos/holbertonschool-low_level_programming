1bc1db8ec076cb78cf2fc7b34c9c60b500255a61
YanielRamos <yanielramos0504@gmail.com>
2023-03-10 16:49:35 +0000

    adding file 3

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -2,8 +2,8 @@
 
 /**
  * main - program that prints the number of arguents
- *
- *
+ *@argc: argument counter
+ *@argv: argument vector
  * Return: 0 for succes
  */
 
@@ -12,6 +12,8 @@ int main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
+	{
 	printf("%s\n", argv[i]);
-	return 0;
+	}
+	return (0);
 }
