d40dc2f7faf6aac229fe11b43c3e01f84069f6cc
YanielRamos <yanielramos0504@gmail.com>
2023-03-13 20:53:50 +0000

    adding file 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,9 +16,13 @@ char *str_concat(char *s1, char *s2)
 	int len1;
 	int len2;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+	       s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	len1 = strlen(s1);
