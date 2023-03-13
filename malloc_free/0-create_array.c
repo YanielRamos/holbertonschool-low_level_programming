2e36e03688e9f80d3627859434083d29d26a9ae5
YanielRamos <yanielramos0504@gmail.com>
2023-03-13 18:45:48 +0000

    adding 0 file

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - function that creates an array
+ * @size: 
+ * @c: 
+ * Return: Null to end
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+	if (size == 0)
+	{
+		return (NULL);	
+	} 
+
+	arr =(char *) malloc(size * sizeof(char));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
