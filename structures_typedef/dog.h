ff7ef7445bc19352b706b7b53a4ebef46b6cb575
YanielRamos <yanielramos0504@gmail.com>
2023-03-20 20:21:55 +0000

    adding dog file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,7 +6,7 @@
  * @age: age
  * @owner: owner
  */
-typedef struct dog dog_t
+struct dog
 {
 	char *name;
 	float age;
@@ -14,4 +14,9 @@ typedef struct dog dog_t
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - new name of the struct
+ */
+typedef struct dog dog_t;
 #endif
