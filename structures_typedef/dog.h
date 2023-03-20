6c387a31505ce2b3e6f14a2b2712787e61e7b154
YanielRamos <yanielramos0504@gmail.com>
2023-03-20 19:16:21 +0000

    adding the ; to the dog.h file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog{
+/**
+ * struct dog - structure of the dog header file
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
