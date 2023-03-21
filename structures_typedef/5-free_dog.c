4f27cafe1cd628dd6a56eabee16d95b23992db0c
YanielRamos <yanielramos0504@gmail.com>
2023-03-21 15:19:55 +0000

    adding file 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: points to function that need to be free
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
