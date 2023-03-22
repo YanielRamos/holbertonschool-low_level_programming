f00dee4d419e8d4ac96860670cde3b36615376ca
YanielRamos <yanielramos0504@gmail.com>
2023-03-22 02:54:45 +0000

    adding files

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: string to be printed
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char*))
+{
+	f(name);
+}
