389e6d528d6478d7303af895d7b74ee1301877ac
YanielRamos <yanielramos0504@gmail.com>
2023-03-27 19:06:24 +0000

    adding file 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,54 +2,45 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
-/** print_all - function that prints anything
+/**
+ * print_all - function that prints anything
  * @format: string with the content
  * Return: yan that have the result
  */
+
 void print_all(const char * const format, ...)
 {
 	va_list yan;
-
-	int i;
+	int i, b;
 	char c;
 	float f;
-	const char *s;
-	int b;
-	const char *y;
-	
+	const char *s, *y;
+
 	va_start(yan, format);
 	b = 1;
 	y = format;
 
 	while (*y != '\0')
 	{
-		 if (!b)
-                {
-                        printf(", ");
-                }
-		b = 0;
-
-		switch (*y){
-			case 'c':
-				c = va_arg(yan, int);
-				printf("%c", c);
-				break;
-			case 'i':
-					i = va_arg(yan, int);
-					printf("%d", i);
-					break;
-			case 'f':
-					f = va_arg(yan, double);
-					printf("%f", f);
-					break;
-			case 's':
-					s = va_arg(yan, char *);
-					printf("%s", s == NULL ? "(nil)": s);
-					break;
-			default:
-					b = 1;
-					break;
+		if (!b)
+		{
+			printf(", ");
 		}
+		b = 0;
+			switch (*y)
+			{
+			case 'c':
+				c = va_arg(yan, int); printf("%c", c); break;
+			case 'i':
+				i = va_arg(yan, int); printf("%d", i); break;
+			case 'f':
+				f = va_arg(yan, double); printf("%f", f); break;
+			case 's':
+				s = va_arg(yan, char *); printf("%s", s == NULL ? "(nil)" : s); break;
+			default:
+				b = 1;
+				break;
+			}
 		y++;
 	}
 	printf("\n");
