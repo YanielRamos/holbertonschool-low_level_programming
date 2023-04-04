80ddcc470fbe78bd7bcb419e0b6e4376b7e1b296
YanielRamos <yanielramos0504@gmail.com>
2023-04-04 19:14:14 +0000

    adding file

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning
+ * @head: list with elements
+ * @str: string to be printed
+ * Return: address of the new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+        list_t *new_node = malloc(sizeof(list_t));
+        size_t count = 0;
+	list_t *current;
+
+        if (new_node == NULL)
+        {
+                return (NULL);
+        }
+
+        new_node->str = strdup(str);
+
+        if (new_node->str == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
+
+        while (str[count] != '\0')
+	{		
+		count++;
+	}
+
+        new_node->len = count;
+        new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (NULL);
+	}
+
+
+       
+	current	= *head;
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+
+	current->next = new_node;
+
+        return (new_node);
+}
