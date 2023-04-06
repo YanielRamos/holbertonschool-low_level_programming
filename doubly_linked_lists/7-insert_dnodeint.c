9922e0662a93a548736b2f2d21126ed1fcf7f073
YanielRamos <yanielramos0504@gmail.com>
2023-04-06 17:32:34 +0000

    adding file 7 fixed

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,6 +10,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+
 	dlistint_t *new_node, *temp, *temp2;
 	size_t finder = 0;
 
@@ -18,29 +19,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node = *h;
-	temp = *h;
-	temp2 = *h;
 	new_node->n = n;
 
-	while (new_node != NULL && finder != idx)
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h == NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	temp = *h;
+
+	while (temp != NULL && finder < idx)
 	{
 		temp = temp->next;
-		temp2 = temp2->next;
 		finder++;
 	}
 
-	temp = temp->prev;
-	temp2 = temp2->next;
-
-	if (temp && temp2 == NULL)
+	if (finder != idx)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
+	temp2 = temp->next;
 	new_node->prev = temp;
 	new_node->next = temp2;
 
-	temp->next = new_node;
-	temp2->prev = new_node;
+	if (temp2 == NULL)
+		temp->next = new_node;
 
+	temp->next = new_node;
 	return (new_node);
 }
