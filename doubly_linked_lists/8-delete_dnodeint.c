6e936a4e9566c14abac2db2d6dc0dfedae1c37e7
YanielRamos <yanielramos0504@gmail.com>
2023-04-06 18:58:10 +0000

    adding file 8 fixed

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,12 +4,13 @@
  * delete_dnodeint_at_index - function that deletes node at index
  * @head: list with elements
  * @index: position of node
- * Return: 1 if it succeeded or -1if it failed
+ * Return: 1 if it succeeded or -1 if it failed
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *actual_node, *prev_node;
+	size_t i = 0;
 
 	actual_node = *head;
 	prev_node = NULL;
@@ -21,17 +22,18 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (actual_node != NULL && index > 0)
+	while (actual_node != NULL && index != i)
 	{
 		prev_node = actual_node;
 		actual_node = actual_node->next;
-		index--;
+		i++;
 	}
 
 	if (actual_node == NULL)
 		return (-1);
 
 	prev_node->next = actual_node->next;
+	actual_node->next->prev = prev_node;
 	free(actual_node);
 	return (1);
 }
