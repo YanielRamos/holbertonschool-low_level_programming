e83594d800b4faafea9e0976323795ac9732e8ef
YanielRamos <yanielramos0504@gmail.com>
2023-04-06 15:00:31 +0000

    adding file 5 fixed

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,17 +12,16 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int finder = 0;
 	dlistint_t *node;
 
-	while (finder != index)
+	node = head;
+
+	while (node != NULL && finder != index)
 	{
-		head = head->next;
+		node = node->next;
 		finder++;
 	}
-
-	node = head;
 
 	if (node == NULL)
 		return (NULL);
 
-	head->n = finder;
 	return (node);
 }
