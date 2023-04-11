854f18f88a9816260a4ee47e4855859a0ab0ba6f
YanielRamos <yanielramos0504@gmail.com>
2023-04-11 18:56:54 +0000

    adding file 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,30 +8,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nread;
-    char buf[1024];
+	int fd;
+	ssize_t nread;
+	char buf[1024];
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    nread = read(fd, buf, letters);
-    if (nread == -1)
-    {
-        close(fd);
-        return (0);
-    }
+	nread = read(fd, buf, letters);
+	if (nread == -1)
+	{
+		close(fd);
+		return (0);
+	}
 
-    if (write(STDOUT_FILENO, buf, nread) != nread)
-    {
-        close(fd);
-        return (0);
-    }
+	if (write(STDOUT_FILENO, buf, nread) != nread)
+	{
+		close(fd);
+		return (0);
+	}
 
-    close(fd);
-    return (nread);
+	close(fd);
+	return (nread);
 }
