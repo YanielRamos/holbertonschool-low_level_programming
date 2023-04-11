ce4231ebd4bfc413e3b1698a85468553317ca9f0
YanielRamos <yanielramos0504@gmail.com>
2023-04-11 18:56:07 +0000

    adding file 0, readme and main.h

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ * Return: actual number of letters read and printed, or 0 if an error occurred
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    ssize_t nread;
+    char buf[1024];
+
+    if (filename == NULL)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    nread = read(fd, buf, letters);
+    if (nread == -1)
+    {
+        close(fd);
+        return (0);
+    }
+
+    if (write(STDOUT_FILENO, buf, nread) != nread)
+    {
+        close(fd);
+        return (0);
+    }
+
+    close(fd);
+    return (nread);
+}
---
+++ file_io/README.md
@@ -0,0 +1,2 @@
+This project is about File I\O' >> README.md
+
