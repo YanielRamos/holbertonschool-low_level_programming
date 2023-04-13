60ab9e51ceb02a2ea6dc782f3e6b508565ad7738
YanielRamos <yanielramos0504@gmail.com>
2023-04-13 18:35:15 +0000

    adding file 0 fixed

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,27 +9,28 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *ht = NULL;
-        unsigned long int i;
+	hash_table_t *ht = NULL;
+	unsigned long int i;
 
-        if (size == 0)
-                return (NULL);
+	if (size == 0)
+		return (NULL);
 
-        ht = malloc(sizeof(hash_table_t));
+	ht = malloc(sizeof(hash_table_t));
 
-        if (ht == NULL)
-                return (NULL);
+	if (ht == NULL)
+		return (NULL);
 
-        ht->size = size;
-        ht->array = malloc(sizeof(hash_node_t *) * size);
-        if (ht->array == NULL)
-        {
-                free(ht);
-                return (NULL);
-        }
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 
-        for (i = 0; i < size; i++)
-                ht->array[i] = NULL;
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
 
-        return (ht);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }
