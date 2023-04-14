24db900108fb3f8b9d43ce687a5e96fd22110906
YanielRamos <yanielramos0504@gmail.com>
2023-04-14 18:47:52 +0000

    adding file 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,25 +16,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
-
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[len] != '\0')
+	if (text_content != NULL)
 	{
-		len++;
+		while (text_content[len] != '\0')
+			len++;
+		wrlen = write(fd, text_content, len);
+		if (wrlen == -1 || wrlen != len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-
-	wrlen = write(fd, text_content, len);
-
-	if (wrlen == -1 || wrlen != len)
-	{
-		close(fd);
-		return (-1);
-	}
-
 	close(fd);
-	return (-1);
+	return (1);
 }
