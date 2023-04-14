2a1408c5b40c096dbf1c4376eab6050f49ea7795
YanielRamos <yanielramos0504@gmail.com>
2023-04-14 18:44:21 +0000

    adding file 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,27 +10,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0, bytes_written;
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	int wrlen, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content == NULL)
+		return (-1);
+
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	while (text_content[len] != '\0')
 	{
-		while (text_content[len])
-			len++;
-		bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1 || bytes_written != len)
-		{
-			close(fd);
-			return (-1);
-		}
+		len++;
+	}
+
+	wrlen = write(fd, text_content, len);
+
+	if (wrlen == -1 || wrlen != len)
+	{
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
-	return (1);
+	return (-1);
 }
